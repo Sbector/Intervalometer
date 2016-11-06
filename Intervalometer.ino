10f3af5a9a6b6f86d548be51b963f62827fff706
Sbector <saulbector@gmail.com>
2016-11-05 20:09:10 -0600

    Código Aduino y actualización de readme

---
+++ Intervalometer.ino
@@ -0,0 +1,79 @@
+#include <JeeLib.h>
+
+int outPin = 13;
+int pulseWidth = 0;
+int state;
+int intervalo = 3;
+int disparos;
+
+
+void setup()
+
+{
+
+  pinMode(outPin, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void SendPulse(int pulseWidth)
+{
+  int reps = pulseWidth / 23.6;
+
+  for (int i = 0; i <= reps; i++)
+  {
+    digitalWrite(outPin, HIGH);
+    delayMicroseconds(11);
+    digitalWrite(outPin, LOW);
+    delayMicroseconds(5);
+  }
+
+}
+
+void SendSequence()
+{
+
+  for (int i = 0; i < 2; i++)
+  {
+    SendPulse(2000);
+    delay(27);
+    delayMicroseconds(800);
+    SendPulse(500);
+    delayMicroseconds(1500);
+    SendPulse(500);
+    delayMicroseconds(3500);
+    SendPulse(500);
+    if (i < 1)
+    {
+      delay(63);
+    }
+  }
+}
+
+
+void loop() {
+
+  Serial.println("Intervalo deseado(milisegundos)");
+  while (Serial.available() == 0) { }
+  intervalo = Serial.parseInt();
+  Serial.print("\n Intervalo: ");
+  Serial.print(intervalo);
+  Serial.println(" ms\n\n");
+
+  Serial.println("Disparos a realizar");
+  while (Serial.available() == 0) { }
+  disparos = Serial.parseInt();
+  Serial.print("\n ");
+  Serial.print(disparos);
+  Serial.println(" disparos\n\n");
+
+
+  for (int val = 1; val <= disparos; val ++) {
+    Serial.print("disparo - ");
+    Serial.println(val);
+    SendSequence ();
+    delay(intervalo);
+    
+  }
+  Serial.print("\n");
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Intervalómetro
 
 Un intervalómetro fabricado con arduino.  Sólo compatible con cámaras Nikon operables con el control ML-L3.
+
+En este momento no se ha realizado la aplicación de Android que controlará el dispositivo. El proyecto sigue en proceso de construcción.
+
+Para operar el dispositivo en esta etapa uso una app de android disponible de forma gratuita en playstore "Bluetooth Terminal" (https://play.google.com/store/apps/details?id=ptah.apps.bluetoothterminal). Sirve enviar los comandos necesarios para iniciar la secuencia via serial.
